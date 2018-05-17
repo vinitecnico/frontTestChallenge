acb70ff7e5392b7fc544c80369ec6b37165aeeae
vinicius <vinitecnico@gmail.com>
2018-05-17 00:20:09 -0300

    cração da master page e rotas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+// Components
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent }
+    // { path: 'starwars', component: StarWarsComponent },
+    // { path: 'marvel', component: MarvelComponent },
+    // { path: 'selected-items', component: SelectedItemsComponent }
+];
+
+@NgModule({
+    imports: [
+        CommonModule,
+        RouterModule.forRoot(routes)
+    ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { BlockUIModule } from 'ng-block-ui';
+import { BlockUIHttpModule } from 'ng-block-ui/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
+// Router
+import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    HttpClientModule,
+    BlockUIModule.forRoot(),
+    BlockUIHttpModule.forRoot(),
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
