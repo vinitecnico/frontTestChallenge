6c48b1eacae949200fd993292981011419ece9cb
vinicius rodrigues <vinitecnico@gmail.com>
2018-05-21 23:32:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var port = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/frontTestChallenge/index.html'));
 });
 
 app.listen(port, function () {
