a713b40ffeb423d2b1382c3b98bfbb6d067fc32d
vinicius <vinitecnico@gmail.com>
2018-05-21 23:17:01 -0300

    ajuste index.js rota get /*

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var express = require('express');
+const path = require('path');
 var app = express();
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-app.use(express.static('dist'));
-app.use('/node_modules',  express.static(__dirname + '/node_modules'));
-app.use('/dist',  express.static(__dirname + '/dist/index.html'));
+app.use(express.static(__dirname + '/dist'));
 
-app.listen(port, function() {
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.listen(port, function () {
     console.log('Our app is running on http://localhost:' + port);
 });
