81aa8563b16877247c2b21a5afbf551faff42c4e
vinicius <vinitecnico@gmail.com>
2018-05-24 22:30:39 -0300

    ajuste active header

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 import { LocalStorageService } from '../../services/local-storage.service';
+
 
 @Component({
     selector: 'abe-nav-bar',
     templateUrl: './nav-bar.component.html'
 })
 export class NavBarComponent {
+    activeUrl: string;
     isCollapsed = true;
     public currentLang: string;
 
     constructor(private translate: TranslateService,
-        private localStorageService: LocalStorageService) {
+        private localStorageService: LocalStorageService,
+        private router: Router) {
         this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
             this.currentLang = event.lang;
+        });
+
+        router.events.subscribe((val: NavigationEnd) => {
+            this.activeUrl = val.url;
         });
     }
 
